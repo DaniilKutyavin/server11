554e08f930587111f5c42b722260e66f29c2c849
DaniilKutyavin <daniilkkutyavin@yandex.ru>
2025-01-08 22:43:14 +0500

    gasgas

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -28,7 +28,7 @@ class OrderController {
       res.status(201).json(order);
       setImmediate(async () => {
         try {
-          await MailService.sendOrderNotification("danya.kutyavin00@mail.ru", order);
+          await MailService.sendOrderNotification("asatryan199405@mail.ru", order);
         } catch (error) {
           console.error("Ошибка отправки письма:", error.message);
         }
@@ -116,7 +116,7 @@ class OrderController {
       res.status(201).json({ message: 'Заказ успешно создан', order: newOrder });
       setImmediate(async () => {
         try {
-          const adminEmail = "danya.kutyavin00@mail.ru";
+          const adminEmail = "asatryan199405@mail.ru";
   
           await MailService.sendOrderNotification(adminEmail, {
             ...newOrder,
